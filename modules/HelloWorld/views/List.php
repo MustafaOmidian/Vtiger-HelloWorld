bd4ef85d1a8c84896d85c72015aeec507ee322c5
Mustafa Omidian <60924400+MustafaOmidian@users.noreply.github.com>
2023-04-23 05:54:24 +0330

    Update List.php

--- modules/HelloWorld/views/List.php
+++ modules/HelloWorld/views/List.php
@@ -3,5 +3,5 @@ class HelloWorld_List_View extends Vtiger_Index_View {
 
         public function process(Vtiger_Request $request) {
                 $viewer = $this->getViewer($request);
-                $viewer->view('List.tpl', $request->getModule());
+                $viewer->view('List.tpl', $request->getModule());}
         }
